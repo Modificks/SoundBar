0f4f989ab74288a4f3bf9165b36a4ae66051e190
Modificks <dimanakonechnui7@gmail.com>
2024-02-27 13:47:33 +0200

    Initial commit

---
+++ src/main/java/Web/Player/SoundBar/SoundBarApplication.java
@@ -0,0 +1,12 @@
+package Web.Player.SoundBar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundBarApplication.class, args);
+	}
+}
