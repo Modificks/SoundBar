c6db79f2c66ce75ec4fc868728b47ab2b71a98df
Modificks <dimanakonechnui7@gmail.com>
2024-02-29 16:44:46 +0200

    adding user controller

---
+++ src/main/java/Web/Player/SoundBar/APIs/UserController.java
@@ -0,0 +1,56 @@
+package Web.Player.SoundBar.APIs;
+
+import Web.Player.SoundBar.Domains.Entities.User;
+import Web.Player.SoundBar.Domains.Entities.UserRole;
+import Web.Player.SoundBar.Services.Impl.RefreshTokenServiceImpl;
+import Web.Player.SoundBar.Services.Impl.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserServiceImpl userServiceImpl;
+
+    private final RefreshTokenServiceImpl refreshTokenServiceImpl;
+
+    //TODO: manage apis and create User DTO
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userServiceImpl.savaUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<UserRole> saveRole(@RequestBody UserRole userRole) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userServiceImpl.saveRole(userRole));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userServiceImpl.getUsers());
+    }
+
+
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        refreshTokenServiceImpl.refreshToken(request, response);
+    }
+}
