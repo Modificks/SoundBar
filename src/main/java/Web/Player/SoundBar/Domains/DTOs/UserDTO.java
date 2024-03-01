b98aa32e564e23d1be55490924f508b5044e7326
Modificks <dimanakonechnui7@gmail.com>
2024-03-01 18:24:04 +0200

    adding custom annotations

--- src/main/java/Web/Player/SoundBar/Domains/DTOs/UserDTO.java
+++ src/main/java/Web/Player/SoundBar/Domains/DTOs/UserDTO.java
@@ -1,9 +1,11 @@
 package Web.Player.SoundBar.Domains.DTOs;
 
+import Web.Player.SoundBar.Annotations.Nickname;
 import Web.Player.SoundBar.Domains.Entities.PlayList;
 import Web.Player.SoundBar.Domains.Entities.UserRole;
 import lombok.Getter;
 import lombok.Setter;
+import javax.validation.constraints.Email;
 import java.util.Set;
 
 @Getter
@@ -11,8 +13,13 @@ import java.util.Set;
 public class UserDTO {
 
     private Long id;
+
+    @Email(message = "Invalid format of email")
     private String email;
+
+    @Nickname(message = "invalid nickname")
     private String nickname;
+
     private String password;
     private Set<UserRole> userRoles;
     private Set<PlayList> playList;
