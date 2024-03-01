d4fee9e50b7a0fd1a0c0570c650bd2cbc9d6fd23
Modificks <dimanakonechnui7@gmail.com>
2024-03-01 17:59:17 +0200

    solving issues with mapper

--- src/main/java/Web/Player/SoundBar/Domains/Mapper/UserMapper.java
+++ src/main/java/Web/Player/SoundBar/Domains/Mapper/UserMapper.java
@@ -3,10 +3,17 @@ package Web.Player.SoundBar.Domains.Mapper;
 import Web.Player.SoundBar.Domains.DTOs.UserDTO;
 import Web.Player.SoundBar.Domains.Entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper extends BaseMapper<User, UserDTO> {
 
+    @Mapping(target = "id", source = "userDTO.id")
+    @Mapping(target = "email", source = "userDTO.email")
+    @Mapping(target = "nickname", source = "userDTO.nickname")
+    @Mapping(target = "password", source = "userDTO.password")
+    @Mapping(target = "userRoles", source = "userDTO.userRoles")
+    @Mapping(target = "playList", source = "userDTO.playList")
     User toEntity(UserDTO userDTO);
 
     UserDTO toDto(User user);
