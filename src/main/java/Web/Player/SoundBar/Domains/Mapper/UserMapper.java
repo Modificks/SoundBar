16e5594951218dfa3d3c83ff448e2d14ce8f850c
Modificks <dimanakonechnui7@gmail.com>
2024-03-12 13:30:16 +0200

    solving StackOverflowError

--- src/main/java/Web/Player/SoundBar/Domains/Mapper/UserMapper.java
+++ src/main/java/Web/Player/SoundBar/Domains/Mapper/UserMapper.java
@@ -3,6 +3,7 @@ package Web.Player.SoundBar.Domains.Mapper;
 import Web.Player.SoundBar.Domains.DTOs.UserDTO;
 import Web.Player.SoundBar.Domains.Entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
@@ -11,5 +12,6 @@ public interface UserMapper extends BaseMapper<User, UserDTO> {
     @Named("toUserDto")
     User toEntity(UserDTO userAuthDTO);
 
+    @Mapping(target = "playList", ignore = true)
     UserDTO toDto(User user);
 }
