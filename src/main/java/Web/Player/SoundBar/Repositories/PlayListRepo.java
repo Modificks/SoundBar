c8579e7b1d321734c488ab1a470643de54f047f8
Modificks <dimanakonechnui7@gmail.com>
2024-03-26 18:04:23 +0200

    Implementing logic for deleting music

--- src/main/java/Web/Player/SoundBar/Repositories/PlayListRepo.java
+++ src/main/java/Web/Player/SoundBar/Repositories/PlayListRepo.java
@@ -4,6 +4,7 @@ import Web.Player.SoundBar.Domains.Entities.PlayList;
 import Web.Player.SoundBar.Domains.Entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface PlayListRepo extends CrudRepository<PlayList, Long> {
@@ -11,4 +12,6 @@ public interface PlayListRepo extends CrudRepository<PlayList, Long> {
     PlayList findByNameAndUser(String playListName, User user);
 
     PlayList findPlayListById(Long playListId);
+
+    List<PlayList> findAll();
 }
--- src/main/java/Web/Player/SoundBar/Services/Impl/SongServiceImpl.java
+++ src/main/java/Web/Player/SoundBar/Services/Impl/SongServiceImpl.java
@@ -2,16 +2,21 @@ package Web.Player.SoundBar.Services.Impl;
 
 import Web.Player.SoundBar.Domains.DTOs.SongBaseDTO;
 import Web.Player.SoundBar.Domains.Entities.Artist;
+import Web.Player.SoundBar.Domains.Entities.PlayList;
 import Web.Player.SoundBar.Domains.Entities.Song;
 import Web.Player.SoundBar.Domains.Mapper.SongMapper;
 import Web.Player.SoundBar.Repositories.ArtistRepo;
+import Web.Player.SoundBar.Repositories.PlayListRepo;
 import Web.Player.SoundBar.Repositories.SongRepo;
 import Web.Player.SoundBar.Services.SongService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +27,8 @@ public class SongServiceImpl implements SongService {
     private final SongRepo songRepo;
 
     private final ArtistRepo artistRepo;
+
+    private final PlayListRepo playListRepo;
 
     private final SongMapper songMapper;
 
@@ -45,5 +52,30 @@ public class SongServiceImpl implements SongService {
         }
 
         return (List<Song>) songRepo.saveAll(songs);
+    }
+
+    public void deleteSong(SongBaseDTO songBaseDTO) {
+        Song song = songRepo.findSongById(songBaseDTO.getId());
+        List<PlayList> playLists = playListRepo.findAll();
+
+        if (song == null) {
+            throw new EntityNotFoundException("Can not find this song");
+        } else {
+
+            Set<Song> songsInPlayList = playLists.stream()
+                    .flatMap(playList -> playList.getPlayListsMusic().stream())
+                    .collect(Collectors.toSet());
+
+            if (songsInPlayList.contains(song)) {
+                songsInPlayList.remove(song);
+
+                playLists.forEach(playList -> {
+                    playList.setPlayListsMusic(songsInPlayList);
+                    playListRepo.save(playList);
+                });
+            }
+            s3ServiceImpl.deleteFileFromS3Bucket(songBaseDTO.getUrl());
+            songRepo.delete(song);
+        }
     }
 }
